c9bfa3a4793a44c2295594c6b465fdde54ce67cc
john <jgrayson@esri.com>
2022-08-25 10:06:47 -0700

    added temporary performance details

--- js/Application.js
+++ js/Application.js
@@ -149,6 +149,9 @@ class Application extends AppBase {
 
       // VIEW READY //
       this.configView(view).then(() => {
+
+        this.displayPerformanceInfo({view});
+
         this.initializeTrendCharts();
         this.initializeCountriesLayer({view});
         this.initializeNorthPole({view});
@@ -160,6 +163,79 @@ class Application extends AppBase {
         }).catch(reject);
       }).catch(reject);
     });
+  }
+
+  displayPerformanceInfo({view}) {
+
+    const updatePerformanceInfo = () => {
+      const performanceInfo = view.performanceInfo;
+      updateMemoryTitle(
+        performanceInfo.usedMemory,
+        performanceInfo.totalMemory,
+        performanceInfo.quality
+      );
+      updateTables(performanceInfo);
+      setTimeout(updatePerformanceInfo, 1000);
+    };
+
+    const title = document.getElementById("title");
+    function updateMemoryTitle(used, total, quality) {
+      title.innerHTML = `Memory: ${ getMB(used) }MB/${ getMB(total) }MB  -  Quality: ${ Math.round(100 * quality) } %`;
+    }
+
+    const tableMemoryContainer = document.getElementById("memory");
+    const tableCountContainer = document.getElementById("count");
+
+    function updateTables(stats) {
+      tableMemoryContainer.innerHTML = `<tr>
+            <th>Resource</th>
+            <th>Memory(MB)</th>
+          </tr>`;
+      for (const layerInfo of stats.layerPerformanceInfos) {
+        const row = document.createElement("tr");
+        row.innerHTML = `<td>${ layerInfo.layer.title }</td><td class="center">${ getMB(layerInfo.memory) }</td>`;
+        tableMemoryContainer.appendChild(row);
+      }
+
+      tableCountContainer.innerHTML = `<tr>
+            <th>Layer - Features</th>
+            <th>Displayed / Max<br>(count)</th>
+            <th>Total<br>(count)</th>
+          </tr>`;
+
+      for (const layerInfo of stats.layerPerformanceInfos) {
+        if (layerInfo.maximumNumberOfFeatures) {
+          const row = document.createElement("tr");
+          row.innerHTML = `<td>${ layerInfo.layer.title }`;
+          row.innerHTML += `<td class="center">${
+            layerInfo.displayedNumberOfFeatures
+              ? layerInfo.displayedNumberOfFeatures
+              : "-"
+          } / ${
+            layerInfo.maximumNumberOfFeatures
+              ? layerInfo.maximumNumberOfFeatures
+              : "-"
+          }</td>`;
+          row.innerHTML += `<td class="center">${
+            layerInfo.totalNumberOfFeatures
+              ? layerInfo.totalNumberOfFeatures
+              : "-"
+          }</td>`;
+          tableCountContainer.appendChild(row);
+        }
+      }
+    }
+
+    function getMB(bytes) {
+      const kilobyte = 1024;
+      const megabyte = kilobyte * 1024;
+      return Math.round(bytes / megabyte);
+    }
+
+    view.when(() => {
+      updatePerformanceInfo();
+    });
+
   }
 
   /**
